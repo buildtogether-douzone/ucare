896f23b3e9c72d3809f41854d0640420edc05017
sinhu-jung <tlsgn26@naver.com>
2021-08-09 17:18:51 +0900

    ucare frontend

---
+++ ucare_frontend/src/Routes.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Route,  BrowserRouter as Router  } from "react-router-dom";
+import SignIn from './view/SignIn';
+import Home from './view/Home';
+
+export default class Routes extends Component {
+    render() {
+        return (
+            <Router>
+                <main>
+                    <Route exact path="/" component={SignIn} />
+                    <Route exact path="/Home" component={Home} />
+                </main>
+            </Router>
+        );
+    }
+}
