10c520cf78ccb4dd05c5519848190095f6dca8df
sinhu-jung <tlsgn26@naver.com>
2021-08-12 15:48:58 +0900

    ucar front sign in

--- ucare_frontend/src/include/Footer.js
+++ ucare_frontend/src/include/Footer.js
@@ -8,8 +8,8 @@ export default function Footer(){
         <Box mt={5}>
             <Typography variant="body2" color="textSecondary" align="center">
                 {'Copyright © '}
-                <Link color="inherit" href="https://material-ui.com/">
-                Your Website
+                <Link color="inherit" href="/">
+                    Build Together
                 </Link>{' '}
                 {new Date().getFullYear()}
                 {'.'}
--- ucare_frontend/src/view/SignIn.js
+++ ucare_frontend/src/view/SignIn.js
@@ -4,27 +4,25 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Footter from '../include/Footer';
+import Footer from '../include/Footer';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: '100vh',
   },
   image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundImage: `url(${require("../assets/image/his#1.jpg")})`,
     backgroundRepeat: 'no-repeat',
     backgroundColor:
       theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
+    backgroundSize: '100% 100%',
+    backgroundPosition: 'left',
   },
   paper: {
     margin: theme.spacing(8, 4),
@@ -51,12 +49,13 @@ export default function SignInSide() {
   return (
     <Grid container component="main" className={classes.root}>
       <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={6} md={7} className={classes.image} />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
+            <Typography component="h1" variant="h3">
+                U-Care
+            </Typography>
+            <LocalHospitalIcon color="secondary" style={{ fontSize: 80 }}/>
             <Typography component="h1" variant="h5">
                 Sign in
             </Typography>
@@ -67,7 +66,7 @@ export default function SignInSide() {
               required
               fullWidth
               id="email"
-              label="Email Address"
+              label="id"
               name="email"
               autoComplete="email"
               autoFocus
@@ -94,22 +93,22 @@ export default function SignInSide() {
                     color="primary"
                     className={classes.submit}
                 >
-                Sign In
+                로그인 하기
                 </Button>
             </Link>
             <Grid container>
               <Grid item xs>
                 <Link href="/_Home" variant="body2">
-                  Forgot password?
+                  비밀번호 찾기
                 </Link>
               </Grid>
               <Grid item>
                 <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
+                  {"회원가입"}
                 </Link>
               </Grid>
             </Grid>
-            <Footter />
+            <Footer />
           </form>
         </div>
       </Grid>
