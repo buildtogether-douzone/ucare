69bd851c862381a2a160c5fb9a309162ee7590e2
sinhu-jung <tlsgn26@naver.com>
2021-08-22 21:39:45 +0900

    ucare frontend url Config

--- ucare_frontend/src/service/receiptService.js
+++ ucare_frontend/src/service/receiptService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Receipt_API_BASE_URL } from './urlConfig';
+import { RECEIPT_API_BASE_URL } from './urlConfig';
 
 class receiptService {
   
@@ -8,7 +8,7 @@ class receiptService {
   // }
 
   retrieveAll(){
-    return axios.get(Receipt_API_BASE_URL + '/retrieveAll');
+    return axios.get(RECEIPT_API_BASE_URL + '/retrieveAll');
   }
 }
 
--- ucare_frontend/src/service/urlConfig.js
+++ ucare_frontend/src/service/urlConfig.js
@@ -1,5 +1,5 @@
 export const USER_API_BASE_URL = "http://localhost:8080/ucare_backend";
-export const Receipt_API_BASE_URL = "http://localhost:8080/ucare_backend/api/receipt";
+export const RECEIPT_API_BASE_URL = "http://localhost:8080/ucare_backend/api/receipt";
 export const PATIENT_API_BASE_URL = "http://localhost:8080/ucare_backend/api/patient";
 export const INFO_API_BASE_URL = "http://localhost:8080/ucare_backend/api/disease";
 export const ADMIN_API_BASE_URL = "http://localhost:8080/ucare_backend/api/admin";
