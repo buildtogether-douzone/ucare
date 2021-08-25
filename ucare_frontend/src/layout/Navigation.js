8a37842bc2a1cc6951d6fb67868f3c8e3c1b1ccc
sinhu-jung <tlsgn26@naver.com>
2021-08-25 16:56:46 +0900

    navigation role admin 조건 추가

--- ucare_frontend/src/layout/Navigation.js
+++ ucare_frontend/src/layout/Navigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import clsx from 'clsx';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import Drawer from '@material-ui/core/Drawer';
@@ -46,6 +46,7 @@ const useStyles = makeStyles((theme) => ({
 
 const Navigation = ( { open, drawerManage }) => {
     const classes = useStyles();
+
     return (
         <Drawer
             variant="permanent"
@@ -60,7 +61,10 @@ const Navigation = ( { open, drawerManage }) => {
                 </IconButton>
             </div>
             <Divider />
-            <List >{sessionStorage.getItem('role') == '간호사' ? nurseListItems : sessionStorage.getItem('role') == '의사' ? doctorListItems : adminListItems }</List>
+            <List >{
+              sessionStorage.getItem('role') == '간호사' ? nurseListItems : 
+              sessionStorage.getItem('role') == '의사' ? doctorListItems : 
+              sessionStorage.getItem('role') == '관리자' && adminListItems }</List>
         </Drawer>
     );
 }
