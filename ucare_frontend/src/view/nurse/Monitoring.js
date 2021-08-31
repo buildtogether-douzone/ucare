78c59848941b0c6b4cc4beb1da416b981eae5cbc
dhcho <oop6413@gmail.com>
2021-08-31 23:43:15 +0900

    진료현황 개발중

--- ucare_frontend/src/view/nurse/Monitoring.js
+++ ucare_frontend/src/view/nurse/Monitoring.js
@@ -9,12 +9,13 @@ import { Calendar } from 'primereact/calendar';
 import '../../assets/scss/DataView.scss';
 
 export default function Monitoring() {
-    const [products, setProducts] = useState(null);
-    const [layout, setLayout] = useState('list');
+    const [items, setItems] = useState(null);
     const [sortKey, setSortKey] = useState(null);
     const [sortOrder, setSortOrder] = useState(null);
     const [sortField, setSortField] = useState(null);
+    const [layout, setLayout] = useState('list');
     const [date, setDate] = useState(new Date());
+    
     const sortOptions = [
         {label: 'Price High to Low', value: '!price'},
         {label: 'Price Low to High', value: 'price'},
@@ -23,7 +24,7 @@ export default function Monitoring() {
     const productService = new ProductService();
 
     useEffect(() => {
-        productService.getProducts().then(data => setProducts(data));
+        productService.getProducts().then(data => setItems(data));
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
     const onSortChange = (event) => {
@@ -60,40 +61,11 @@ export default function Monitoring() {
         );
     }
 
-    const renderGridItem = (data) => {
-        return (
-            <div className="p-col-8">
-                <div className="product-grid-item card">
-                    <div className="product-grid-item-top">
-                        <div>
-                            <i className="pi pi-tag product-category-icon"></i>
-                            <span className="product-category">{data.category}</span>
-                        </div>
-                        <span className={`product-badge status-${data.inventoryStatus.toLowerCase()}`}>{data.inventoryStatus}</span>
-                    </div>
-                    <div className="product-grid-item-content">
-                        <div className="product-name">{data.name}</div>
-                        <div className="product-description">{data.description}</div>
-                        <Rating value={data.rating} readOnly cancel={false}></Rating>
-                    </div>
-                    <div className="product-grid-item-bottom">
-                        <span className="product-price">${data.price}</span>
-                        <Button icon="pi pi-shopping-cart" label="Add to Cart" disabled={data.inventoryStatus === 'OUTOFSTOCK'}></Button>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
-    const itemTemplate = (product, layout) => {
-        if (!product) {
+    const itemTemplate = (item) => {
+        if (!item) {
             return;
         }
-
-        if (layout === 'list')
-            return renderListItem(product);
-        else if (layout === 'grid')
-            return renderGridItem(product);
+        return renderListItem(item);
     }
 
     const renderHeader = () => {
@@ -102,9 +74,6 @@ export default function Monitoring() {
                 <div className="p-col-6" style={{textAlign: 'left'}}>
                     <Dropdown options={sortOptions} value={sortKey} optionLabel="label" placeholder="Sort By Price" onChange={onSortChange}/>
                     <Calendar dateFormat="yy/mm/dd" value={date} onChange={(e) => setDate(e.value)}></Calendar>
-                </div>
-                <div className="p-col-6" style={{textAlign: 'right'}}>
-                    <DataViewLayoutOptions layout={layout} onChange={(e) => setLayout(e.value)} />
                 </div>
             </div>
         );
@@ -116,7 +85,7 @@ export default function Monitoring() {
         <div className="test">
         <div className="dataview">
             <div className="card">
-                <DataView value={products} layout={layout} header={header}
+                <DataView value={items} layout={layout} header={header}
                         itemTemplate={itemTemplate} paginator rows={9}
                         sortOrder={sortOrder} sortField={sortField} />
             </div>
