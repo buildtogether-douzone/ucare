8631376e9eb650feaca6bd597efaecce0d2b00cd
sinhu-jung <tlsgn26@naver.com>
2021-09-06 20:34:16 +0900

    private Route has role

--- ucare_frontend/src/routes/PrivateRoute.js
+++ ucare_frontend/src/routes/PrivateRoute.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 
 export default function PrivateRoute ({ component: Component, role ,...rest}) {
-    console.log(sessionStorage.getItem('role') == role) 
     return (
         <Route
             {...rest}
