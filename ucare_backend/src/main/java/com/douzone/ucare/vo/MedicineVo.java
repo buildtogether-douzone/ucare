8da29ec66a2277df5ae54cb0568cc20cd6ede936
dhcho <oop6413@gmail.com>
2021-09-07 11:43:50 +0900

    약품 엑셀업로드

--- ucare_backend/src/main/java/com/douzone/ucare/vo/MedicineVo.java
+++ ucare_backend/src/main/java/com/douzone/ucare/vo/MedicineVo.java
@@ -2,11 +2,12 @@ package com.douzone.ucare.vo;
 
 public class MedicineVo {
 	private int medicineNo;
+	private String medicineCode;
 	private String medicineNm;
-	private String symptom;
-	private String generic;
-	private int price;
-	private String maker;
+	private String company;
+	private String mainIngredient;
+	private String additive;
+	private String origin;
 	
 	public int getMedicineNo() {
 		return medicineNo;
@@ -14,40 +15,48 @@ public class MedicineVo {
 	public void setMedicineNo(int medicineNo) {
 		this.medicineNo = medicineNo;
 	}
+	public String getMedicineCode() {
+		return medicineCode;
+	}
+	public void setMedicineCode(String medicineCode) {
+		this.medicineCode = medicineCode;
+	}
 	public String getMedicineNm() {
 		return medicineNm;
 	}
 	public void setMedicineNm(String medicineNm) {
 		this.medicineNm = medicineNm;
 	}
-	public String getSymptom() {
-		return symptom;
+	public String getCompany() {
+		return company;
 	}
-	public void setSymptom(String symptom) {
-		this.symptom = symptom;
+	public void setCompany(String company) {
+		this.company = company;
 	}
-	public String getGeneric() {
-		return generic;
+	public String getMainIngredient() {
+		return mainIngredient;
 	}
-	public void setGeneric(String generic) {
-		this.generic = generic;
+	public void setMainIngredient(String mainIngredient) {
+		this.mainIngredient = mainIngredient;
 	}
-	public int getPrice() {
-		return price;
+	public String getAdditive() {
+		return additive;
 	}
-	public void setPrice(int price) {
-		this.price = price;
+	public void setAdditive(String additive) {
+		this.additive = additive;
 	}
-	public String getMaker() {
-		return maker;
+	public String getOrigin() {
+		return origin;
 	}
-	public void setMaker(String maker) {
-		this.maker = maker;
+	public void setOrigin(String origin) {
+		this.origin = origin;
 	}
 	
 	@Override
 	public String toString() {
-		return "MedicineVo [medicineNo=" + medicineNo + ", medicineNm=" + medicineNm + ", symptom=" + symptom
-				+ ", generic=" + generic + ", price=" + price + ", maker=" + maker + "]";
+		return "MedicineVo [medicineNo=" + medicineNo + ", medicineCode=" + medicineCode + ", medicineNm=" + medicineNm
+				+ ", company=" + company + ", mainIngredient=" + mainIngredient + ", additive=" + additive + ", origin="
+				+ origin + "]";
 	}
+	
 }
