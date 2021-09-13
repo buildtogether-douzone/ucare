9931a188866b9d7d3909eb9eef6fe964fda9d363
naolj83 <naolj83@gmail.com>
2021-09-13 15:19:08 +0900

    MessageVo

---
+++ ucare_backend/src/main/java/com/douzone/ucare/vo/MessageVo.java
@@ -0,0 +1,59 @@
+package com.douzone.ucare.vo;
+
+public class MessageVo {
+	private Long msgNo;
+	private String name;
+	private String toName;
+	private String contents;
+	private String msgDate;
+	private String status;
+	private Long userNo;
+
+	public Long getMsgNo() {
+		return msgNo;
+	}
+	public void setMsgNo(Long msgNo) {
+		this.msgNo = msgNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getToName() {
+		return toName;
+	}
+	public void setToName(String toName) {
+		this.toName = toName;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getMsgDate() {
+		return msgDate;
+	}
+	public void setMsgDate(String msgDate) {
+		this.msgDate = msgDate;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Long getUserNo() {
+		return userNo;
+	}
+	public void setUserNo(Long userNo) {
+		this.userNo = userNo;
+	}
+
+	
+
+}
+	
+
