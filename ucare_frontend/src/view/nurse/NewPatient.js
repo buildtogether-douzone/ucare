1a39d14a9b88cde9ab59919b075ce9705d321068
naolj83 <naolj83@gmail.com>
2021-09-28 10:35:45 +0900

    환자등록 이름 정규식

--- ucare_frontend/src/view/nurse/NewPatient.js
+++ ucare_frontend/src/view/nurse/NewPatient.js
@@ -124,6 +124,11 @@ export default function NewPatient() {
       setSSN(ssn.replace(/(\d{6})(\d{7})/, '$1-$2'));
     }
   }, [ssn, telNo, gender, age])
+  
+  const nameRegex = /^[가-힣]{2,15}$/;
+  const nameValidError = () =>
+    name != '' ? (nameRegex.test(name) ? false : true) : false; 
+
 
   const telNoChange = (e) => {
     const regex = /^[0-9\b -]{0,13}$/;
@@ -132,21 +137,15 @@ export default function NewPatient() {
     }
   };
   
-
-
   const ssnChange = (e) => {
     const regex = /^[0-9\b -]{0,13}$/;
     if (regex.test(e.target.value)) {
       setSSN(e.target.value);
     }
   };
-
-
-
-
+  
   const regex = /^((?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]))$/;
-
-  const hasNotValidError = () =>
+  const hasNotValidError = () => 
     email != '' ? (regex.test(email) ? false : true) : false; 
 
 
@@ -272,6 +271,10 @@ export default function NewPatient() {
                 autoComplete="name"
                 value={name}
                 onChange={(e) => { setName(e.target.value) }}
+                error={nameValidError()}
+                helperText={
+                  nameValidError() ? "특수문자,영어,숫자는 사용할수 없습니다." : null
+                }
               />
             </Grid>
             <Grid item xs={12} style={{ border: '1px solid #D6D6D6' }}>
