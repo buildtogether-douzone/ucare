3865e7501fc83a70adce19f72b3e5868c9733d53
naolj83 <naolj83@gmail.com>
2021-10-01 12:03:10 +0900

    hospital css -ing

--- ucare_frontend/src/view/admin/Hospital.js
+++ ucare_frontend/src/view/admin/Hospital.js
@@ -11,6 +11,7 @@ import PrintIcon from '@material-ui/icons/Print';
 import { Button, TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import { InputText } from "primereact/inputtext";
+import { Card } from 'primereact/card';
 import hospitalService from '../../service/hospitalService';
 
 const useStyles = makeStyles({
@@ -148,131 +149,127 @@ export default function Hospital() {
 
     return (
         <React.Fragment>
-            <div style={{ marginLeft:'10%', marginTop: '40px', display:'flex', flexDirection:'column', float:'left'}} >
-                <h2>병원 정보</h2>
-                <div className={classes.Line}>
-                    <div className={classes.addon}>
-                        <BusinessIcon style={{ fontSize: "25px", color: "#616161" }} />
-                    </div>
-                    <InputText 
-                        placeholder="병원명" 
-                        className={classes.textStyle} 
-                        value={hospitalName || ''} 
-                        onChange={(e) => setHospitalName(e.target.value)}  />
-                </div>
-                <div className={classes.Line}>
-                    <div className={classes.addon}>
-                        <PersonIcon style={{ fontSize: "25px", color: "#616161" }} />
-                    </div>
-                    <InputText 
-                        placeholder="병원장명" 
-                        className={classes.textStyle} 
-                        value={headName || ''}
-                        onChange={(e) => setHeadName(e.target.value)}/>
-                </div>
-                <div className={classes.Line}>
-                    <div className={classes.addon}>
-                        <HomeIcon style={{ fontSize: "25px", color: "#616161" }} />
-                    </div>
-                    <InputText 
-                        placeholder="병원 주소" 
-                        className={classes.textStyle} 
-                        value={address || ''}
-                        onChange={(e) => setAddress(e.target.value)}/>
-                </div>
-                <div className={classes.Line}>
-                    <div className={classes.addon}>
-                        <PhoneIcon style={{ fontSize: "25px", color: "#616161" }} />
-                    </div>
-                    <InputText 
-                        placeholder="병원 전화번호" 
-                        className={classes.textStyle}
-                        value={telNo || ''} 
-                        onChange={telNoChange}/>
-                </div>
-                <div className={classes.Line}>
-                    <div className={classes.addon}>
-                        <AttachMoneyIcon style={{ fontSize: "25px", color: "#616161" }} />
-                    </div>
-                    <InputText 
-                        placeholder="기본 진료비" 
-                        className={classes.textStyle} 
-                        value={ basicPrice || '' }
-                        onChange={(e) => setBasicPrice(e.target.value)}/>
-                </div>
-                <div className={classes.Line}>
-                    <div className={classes.addon}>
-                        <LanguageIcon style={{ fontSize: "25px", color: "#616161" }} />
-                    </div>
-                    <InputText 
-                        placeholder="사이트 주소" 
-                        className={classes.textStyle} 
-                        value={ siteAddress || '' }
-                        onChange={(e) => setSiteAddress(e.target.value)}/>
-                </div>
-                <div className={classes.Line}>
-                    <div className={classes.addon}>
-                        <AlternateEmailIcon style={{ fontSize: "25px", color: "#616161" }} />
-                    </div>
-                    <InputText 
-                        placeholder="이메일 주소" 
-                        className={classes.textStyle} 
-                        value={ email || '' }
-                        onChange={(e) => setEmail(e.target.value)}/>
-                </div>
-                <div className={classes.Line}>
-                    <div className={classes.addon}>
-                        <PrintIcon style={{ fontSize: "25px", color: "#616161" }} />
-                    </div>
-                    <InputText 
-                        placeholder="팩스 번호" 
-                        className={classes.textStyle} 
-                        value={ faxNo || '' }
-                        onChange={(e) => setFaxNo(e.target.value)}/>
-                </div>
-                <Button
-                    style={{backgroundColor:'#616161' }}
-                    variant="contained"
-                    color="primary"
-                    type="submit"
-                    onClick={saveInfo}
-                >
-                    등록하기
-                </Button>
-            </div>
-            <div style={{ marginTop: '40px', marginLeft: '40px', float:'left' }}>
-                <div style={{ overflow: 'hidden' }}>
-                    <h2>병원장 사진</h2>
-                    <div className={classes.image} >
-                        <div className={classes.profile}
-                            style={previewURL == null ?
-                                { backgroundImage: `url(${require("../../assets/image/profile.jpg")})` }
-                                : { backgroundImage: `url(${previewURL})` }
-                            } />
-                        <Button
-                            className={classes.button}
-                            variant="contained"
-                            color="default"
-                            component="label"
-                            startIcon={
-                                <span>
-                                    <CloudUploadIcon style={{ padding: '5px 0 0 0' }} /><span style={{ padding: '0 0 0px 2px' }}>Upload</span>
+            <div className="p-grid" style={{ margin: '10px' }}>
+                <div className="p-col-12">
+                    <div className="card p-fluid">
+                        <Card title="병원 정보">
+                        <div className="p-col-12 p-lg-6">
+                            <div className="p-field">
+                                <span className="p-input-icon-left">
+                                    <i className="pi pi-home" />
+                                    <InputText 
+                                        placeholder="병원명" 
+                                        value={hospitalName || ''} 
+                                        onChange={(e) => setHospitalName(e.target.value)}  />
                                 </span>
-                            }>
-                            <input id={"file-input"} style={{ display: 'none' }} type="file" name="imageFile" onChange={handleFileOnChange} />
-                        </Button>
-                    </div>
+                            </div>
+                            <div className="p-field">
+                                <span className="p-input-icon-left">
+                                    <i className="pi pi-user" />
+                                    <InputText 
+                                        placeholder="병원장명" 
+                                        value={headName || ''}
+                                        onChange={(e) => setHeadName(e.target.value)}/>
+                                </span>
+                            </div>
+                            <div className="p-field">
+                                <span className="p-input-icon-left">
+                                    <i className="pi pi-map-marker" />
+                                    <InputText 
+                                        placeholder="병원 주소" 
+                                        value={address || ''}
+                                        onChange={(e) => setAddress(e.target.value)}/>
+                                </span>
+                            </div>
+                            <div className="p-field">
+                                <span className="p-input-icon-left">
+                                    <i className="pi pi-phone" />
+                                    <InputText 
+                                        placeholder="병원 전화번호" 
+                                        value={telNo || ''} 
+                                        onChange={telNoChange}/>
+                                </span>
+                            </div>
+                            <div className="p-field">
+                                <span className="p-input-icon-left">
+                                    <i className="pi pi-dollar" />
+                                    <InputText 
+                                        placeholder="기본 진료비" 
+                                        value={ basicPrice || '' }
+                                        onChange={(e) => setBasicPrice(e.target.value)}/>
+                                </span>
+                            </div>
+                            <div className="p-field">
+                                <span className="p-input-icon-left">
+                                    <i className="pi pi-globe" />
+                                    <InputText 
+                                        placeholder="사이트 주소" 
+                                        value={ siteAddress || '' }
+                                        onChange={(e) => setSiteAddress(e.target.value)}/>
+                                </span>
+                            </div>
+                            <div className="p-field">
+                                <span className="p-input-icon-left">
+                                <i className="pi pi-envelope" />
+                                    <InputText 
+                                        placeholder="이메일 주소" 
+                                        value={ email || '' }
+                                        onChange={(e) => setEmail(e.target.value)}/>
+                                </span>
+                            </div>
+                            <div className="p-field">
+                                <span className="p-input-icon-left">
+                                    <i className="pi pi-print" />
+                                    <InputText 
+                                        placeholder="팩스 번호" 
+                                        value={ faxNo || '' }
+                                        onChange={(e) => setFaxNo(e.target.value)}/>
+                                </span>
+                            </div>
+                            <Button
+                                style={{ backgroundColor:'#1C91FB', border: '1px solid #1C91FB' }}
+                                variant="contained"
+                                color="primary"
+                                type="submit"
+                                onClick={saveInfo}>
+                                등록
+                            </Button>
+                            </div>
+                            <div className="p-col-12 p-lg-4">
+                        <div style={{ overflow: 'hidden' }}>
+                            <h2>병원장 사진</h2>
+                            <div className={classes.image} >
+                                <div className={classes.profile}
+                                    style={previewURL == null ?
+                                        { backgroundImage: `url(${require("../../assets/image/profile.jpg")})` }
+                                        : { backgroundImage: `url(${previewURL})` }
+                                    } />
+                                <Button
+                                    className={classes.button}
+                                    variant="contained"
+                                    color="default"
+                                    component="label"
+                                    startIcon={
+                                        <span>
+                                            <CloudUploadIcon style={{ padding: '5px 0 0 0' }} /><span style={{ padding: '0 0 0px 2px' }}>Upload</span>
+                                        </span>
+                                    }>
+                                    <input id={"file-input"} style={{ display: 'none' }} type="file" name="imageFile" onChange={handleFileOnChange} />
+                                </Button>
+                            </div>
+                        </div>
+                        <div >
+                            <TextField
+                                label="내용"
+                                variant="outlined"
+                                multiline
+                                rows={7}
+                                value={headSpeak || ''}
+                                onChange={(e) => setHeadSpeak(e.target.value)} />
+                        </div>
+                        </div>
+                    </Card>
                 </div>
-                <div >
-                    <h2>병원장 인사말</h2>
-                    <TextField
-                        className={classes.textfiled}
-                        label="내용"
-                        variant="outlined"
-                        multiline
-                        rows={7}
-                        value={headSpeak || ''}
-                        onChange={(e) => setHeadSpeak(e.target.value)} />
                 </div>
             </div>
         </React.Fragment>
