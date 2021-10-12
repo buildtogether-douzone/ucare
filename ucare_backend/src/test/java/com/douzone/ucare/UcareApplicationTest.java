099c3f0e4922a8f2dd87d36004a5db40ff8a3ad5
sinhu-jung <tlsgn26@naver.com>
2021-10-12 12:35:18 +0900

    TDD post(login)

--- ucare_backend/src/test/java/com/douzone/ucare/UcareApplicationTest.java
+++ ucare_backend/src/test/java/com/douzone/ucare/UcareApplicationTest.java
@@ -2,19 +2,25 @@ package com.douzone.ucare;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @WebAppConfiguration
@@ -24,6 +30,9 @@ public class UcareApplicationTest {
 	@Autowired
 	protected MockMvc mockMvc;
 	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
 	@Test 
 	@WithMockUser(roles="관리자")
 	@DisplayName("유저 전체조회 테스트") 
@@ -32,4 +41,20 @@ public class UcareApplicationTest {
 			   .andExpect(status().isOk()) 
 			   .andExpect(content().string(containsString("111")))
 			   .andDo(print()); }
+	
+	@Test
+	@DisplayName("로그인 테스트")
+	void login() throws Exception{
+		
+		Map<String, String> input = new HashMap<>();
+	    input.put("id", "111");
+	    input.put("password", "11111");
+		
+		mockMvc.perform(post("/login")
+			.contentType(MediaType.APPLICATION_JSON) 
+	        .content(objectMapper.writeValueAsString(input)))
+			.andExpect(status().isOk())
+			.andDo(print());
+	}
+	
 }
